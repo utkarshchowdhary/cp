5f8d7bb27083de4b2fa2c7b1a88917271eed9a12
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2021-06-27 11:20:10 +0530

    refactored minimum absolute difference queries utilizing the concept of prefix sum

--- src/main/java/com/github/array/MinAbsDiff.java
+++ src/main/java/com/github/array/MinAbsDiff.java
@@ -11,6 +11,7 @@ public class MinAbsDiff {
         int n = sc.nextInt();
         int[] a = new int[n];
 //        int [] a = {4, 5, 2, 2, 7, 10};
+//        int n = a.length;
 
         System.out.println("enter values");
         for (int i = 0; i < n; i++) {
@@ -31,18 +32,46 @@ public class MinAbsDiff {
             }
         }
 
+//        int[] r = new int[m];
+
+//        for (int i = 0; i < m; i++) {
+//            int from = queries[i][0]; // start index
+//            int to = queries[i][1]; // end index
+//            int min = Integer.MAX_VALUE;
+//            for (int j = from; j <= to; j++) {
+//                for (int k = j + 1; k <= to && a[j] != a[k]; k++) {
+//                    int diff = Math.abs(a[j] - a[k]);
+//                    if (min > diff) min = diff;
+//                }
+//            }
+//            if (min == Integer.MAX_VALUE) r[i] = -1;
+//            else r[i] = min;
+//        }
+
+        int max = Arrays.stream(a).max().getAsInt();
+        int[][] count = new int[n][max + 1]; // cumulative frequencies
         int[] r = new int[m];
+
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < max + 1; j++) {
+                if (i > 0) count[i][j] = count[i - 1][j]; // copy previous row
+            }
+            ++count[i][a[i]];
+        }
 
         for (int i = 0; i < m; i++) {
             int from = queries[i][0]; // start index
             int to = queries[i][1]; // end index
             int min = Integer.MAX_VALUE;
-            for (int j = from; j <= to; j++) {
-                for (int k = j + 1; k <= to && a[j] != a[k]; k++) {
-                    int diff = Math.abs(a[j] - a[k]);
-                    if (min > diff) min = diff;
+            int prev = -1;
+
+            for (int j = 0 ; j < max + 1; j++) {
+                if ((count[to][j] - (from > 0 ? count[from - 1][j] : 0)) > 0) {
+                    if (prev != -1) min = Math.min(min, j - prev);
+                    prev = j;
                 }
             }
+
             if (min == Integer.MAX_VALUE) r[i] = -1;
             else r[i] = min;
         }
