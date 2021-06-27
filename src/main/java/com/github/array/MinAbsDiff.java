7ada5236515ed01690a310806eff04b7b7a7edb5
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2021-06-27 16:03:08 +0530

    added code comments

--- src/main/java/com/github/array/MinAbsDiff.java
+++ src/main/java/com/github/array/MinAbsDiff.java
@@ -7,30 +7,30 @@ public class MinAbsDiff {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("enter arr length ");
-        int n = sc.nextInt();
-        int[] a = new int[n];
-//        int [] a = {4, 5, 2, 2, 7, 10};
-//        int n = a.length;
+//        System.out.print("enter arr length ");
+//        int n = sc.nextInt();
+//        int[] a = new int[n];
+        int [] a = {4, 5, 2, 2, 7, 10};
+        int n = a.length;
 
-        System.out.println("enter values");
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
-        }
+//        System.out.println("enter values");
+//        for (int i = 0; i < n; i++) {
+//            a[i] = sc.nextInt();
+//        }
 
-        System.out.println("enter queries arr length");
-        int m = sc.nextInt();
-        int[][] queries = new int[m][2];
+//        System.out.println("enter queries arr length");
+//        int m = sc.nextInt();
+//        int[][] queries = new int[m][2];
 
-//        int[][] queries = {{2, 3}, {0, 2}, {0 , 5}, {3, 5}};
-//        int m = queries.length;
+        int[][] queries = {{2, 3}, {0, 2}, {0 , 5}, {3, 5}};
+        int m = queries.length;
 
-        System.out.println("enter values");
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < 2; j++) {
-                queries[i][j] = sc.nextInt();
-            }
-        }
+//        System.out.println("enter values");
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < 2; j++) {
+//                queries[i][j] = sc.nextInt();
+//            }
+//        }
 
 //        int[] r = new int[m];
 
@@ -56,7 +56,7 @@ public class MinAbsDiff {
             for(int j = 0; j < max + 1; j++) {
                 if (i > 0) count[i][j] = count[i - 1][j]; // copy previous row
             }
-            ++count[i][a[i]];
+            count[i][a[i]]++; // increment the frequency column associated with array value
         }
 
         for (int i = 0; i < m; i++) {
@@ -66,6 +66,11 @@ public class MinAbsDiff {
             int prev = -1;
 
             for (int j = 0 ; j < max + 1; j++) {
+                /*
+                * if subtraction between the frequencies of end row index and one before start row index
+                * or in case the range starts from beginning the frequencies of end row index are
+                * greater than zero this implies the column index value must exists within the range
+                * */
                 if ((count[to][j] - (from > 0 ? count[from - 1][j] : 0)) > 0) {
                     if (prev != -1) min = Math.min(min, j - prev);
                     prev = j;
