f4f58cddabcc19d6b9302c70a2de30ca420ac111
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2021-07-09 13:27:14 +0530

    map representing items selected with there frequencies for unbounded knapsack

--- src/main/java/dp/KnapsackUnbounded.java
+++ src/main/java/dp/KnapsackUnbounded.java
@@ -3,6 +3,8 @@
 * */
 package main.java.dp;
 
+import java.util.LinkedHashMap;
+
 public class KnapsackUnbounded {
     public static void main(String[] args) {
         int c = 4; // knapsack capacity
@@ -21,10 +23,28 @@ public class KnapsackUnbounded {
             }
         }
 
+        int size = c;
+        LinkedHashMap<Integer, Integer> sel = new LinkedHashMap<>();
+
+        for (int i = n; i > 0; i--) {
+            if (DP[i][size] != DP[i - 1][size]) {
+                int count = 0;
+
+                while (DP[i][size] != DP[i - 1][size]) {
+                    count++;
+                    size -= W[i - 1];
+                }
+
+                sel.put(i - 1, count);
+            }
+        }
+
         // 0, 0, 0, 0, 0
         // 0, 1500, 3000, 4500, 6000
         // 0, 1500, 3000, 4500, 6000
         // 0, 1500, 3000, 4500, 6000
         System.out.println(DP[n][c]);
+
+        System.out.println(sel);
     }
 }
